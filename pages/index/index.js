c658cab10416a0501c3a3715649af61b1d7931ed
xpf <testcontent@163.com>
2019-11-11 14:59:52 +0800

    demos

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,34 @@
 //index.js
 //获取应用实例
-const app = getApp()
+//const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    name: "中国",
+    age: 18,
+    students: [
+      { id: 110, name: 'a', age: 30 },
+      { id: 111, name: 'b', age: 31 },
+      { id: 112, name: 'c', age: 32 },
+      { id: 113, name: 'd', age: 33 },
+    ],
+    counter: 0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  handleBtnClick(){
+    //1.错误的做法
+    //this.data.counter += 1;
+
+    //2。this.setData()
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      counter: this.data.counter + 1
+    });
+  },
+  handleSubClick(){
+    this.setData({
+      counter: this.data.counter - 1
+    });
+  },
+  getUserinfos(event){
+    console.log(event);
   }
 })
